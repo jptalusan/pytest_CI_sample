cd22a994c024f4fd47a5e47e105ab31858b16a3f
JP Talusan <jptalusan@gmail.com>
2024-01-19 11:56:16 -0600

    Trial, forcing an error.

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -5,6 +5,7 @@ from src.area import calculate_area_square
 def test_calculate_area_square():
     assert calculate_area_square(2) == 4
     assert calculate_area_square(2.5) == 6.25
+    assert calculate_area_square(3) == 8
 
 
 def test_calculate_area_square_negative():
